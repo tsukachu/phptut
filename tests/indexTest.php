f69a7cf1a1525acdc0da3ebb8a2936583bee1213
tsukachu <concon.ch.1543@gmail.com>
2017-07-11 14:07:51 +0900

    Add first test

---
+++ tests/indexTest.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+
+class IndexTest extends TestCase {
+  public function setUp() {
+    $this->client = new Client(['base_url'=> 'http://127.0.0.1:8000/']);
+  }
+
+  public function testGetIndex() {
+    $response = $this->client->get('/');
+
+    $this->assertEquals($response->getStatusCode(), '200');
+  }
+}

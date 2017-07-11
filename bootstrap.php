6655f2e63eeebdfba2f730ab59f779aaea60b753
tsukachu <concon.ch.1543@gmail.com>
2017-07-11 14:08:39 +0900

    Add PHPUnit settings

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+// Start the built-in web server in the background.
+$cmd = 'php -t public -S 127.0.0.1:8000 > /dev/null 2>&1 &';
+$descriptorspec = array();
+$pipes = array();
+
+$process = proc_open($cmd, $descriptorspec, $pipes);
+
+// End the built-in web server at the end of the test.
+register_shutdown_function(
+  function () use ($process) {
+    proc_close($process);
+  }
+);

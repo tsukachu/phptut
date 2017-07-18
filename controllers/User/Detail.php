94530ff4b88b205b6075b09319149db1f06f333f
tsukachu <concon.ch.1543@gmail.com>
2017-07-18 23:14:02 +0900

    Change to not use regexp? for get uid

--- controllers/User/Detail.php
+++ controllers/User/Detail.php
@@ -6,21 +6,15 @@ use Controllers\Base;
 
 class Detail extends Base
 {
-    const INDEX_UID = 1;
-
     public static function get()
     {
-        $uid = self::getUid();
+        $uid = self::getUid($_SERVER['REQUEST_URI']);
         echo $uid;
     }
 
-    public static function getUid()
+    public static function getUid($uri)
     {
-        $uri = $_SERVER['REQUEST_URI'];
-        $result = [];
-
-        preg_match(sprintf('/^%s$/', addcslashes('/users/(\d{1,10})/', '/')), $uri, $result);
-        $uid = $result[self::INDEX_UID];
+        $uid = basename($uri);
 
         return (int) $uid;
     }
